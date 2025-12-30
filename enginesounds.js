8bbb57887c871d36b064b5831b321293c2cc6698
ImaGoodGuy,Bro <siancappy@outlook.com>
2025-12-30 08:00:51 -0600

    did stuff

--- enginesounds.js
+++ enginesounds.js
@@ -1,22 +1,22 @@
 
 geofs.aircraft.instance.definition.sounds[0].effects.volume.value = "rpm";
 geofs.aircraft.instance.definition.sounds[0].effects.pitch.value = "rpm";
-geofs.aircraft.instance.definition.sounds[0].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/low.mp3";
+geofs.aircraft.instance.definition.sounds[0].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/low.mp3";
 
 geofs.aircraft.instance.definition.sounds[1].effects.volume.value = "rpm";
 geofs.aircraft.instance.definition.sounds[1].effects.pitch.value = "rpm";
-geofs.aircraft.instance.definition.sounds[1].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/low.mp3";
+geofs.aircraft.instance.definition.sounds[1].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/low.mp3";
 
 geofs.aircraft.instance.definition.sounds[2].effects.volume.value = "rpm";
 geofs.aircraft.instance.definition.sounds[2].effects.pitch.value = "rpm";
-geofs.aircraft.instance.definition.sounds[2].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/low.mp3";
+geofs.aircraft.instance.definition.sounds[2].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/low.mp3";
 
 geofs.aircraft.instance.definition.sounds[5].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/startup.mp3";
 
 // Low layer
 geofs.aircraft.instance.definition.sounds[7] = {};
 geofs.aircraft.instance.definition.sounds[7].id = "rpm4";
-geofs.aircraft.instance.definition.sounds[7].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/low.mp3";
+geofs.aircraft.instance.definition.sounds[7].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/low.mp3";
 geofs.aircraft.instance.definition.sounds[7].effects = { 
     volume: { value: "rpm", ramp: [0, 5000, 9000, 12000], ratio: 1 }, 
     pitch: { value: "rpm", ramp: [0, 20000, 20000, 20000], offset: 1, ratio: 1 } 
@@ -25,7 +25,7 @@ geofs.aircraft.instance.definition.sounds[7].effects = {
 // Mid layer
 geofs.aircraft.instance.definition.sounds[8] = {};
 geofs.aircraft.instance.definition.sounds[8].id = "rpm5";
-geofs.aircraft.instance.definition.sounds[8].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/mid.mp3";
+geofs.aircraft.instance.definition.sounds[8].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/mid.mp3";
 geofs.aircraft.instance.definition.sounds[8].effects = { 
     volume: { value: "rpm", ramp: [9000, 12000, 15000, 18000], ratio: 1 }, 
     pitch: { value: "rpm", ramp: [0, 20000, 20000, 20000], offset: 1, ratio: 1 } 
@@ -34,7 +34,7 @@ geofs.aircraft.instance.definition.sounds[8].effects = {
 // High layer
 geofs.aircraft.instance.definition.sounds[9] = {};
 geofs.aircraft.instance.definition.sounds[9].id = "rpm6";
-geofs.aircraft.instance.definition.sounds[9].file = "https://imagoodguy3.github.io/GeoFS-757-300WL-SFX-Revamp/high.mp3";
+geofs.aircraft.instance.definition.sounds[9].file = "https://imagoodguy3.github.io/GeoFS-GE90-Engine-Sounds/high.mp3";
 geofs.aircraft.instance.definition.sounds[9].effects = { 
     volume: { value: "rpm", ramp: [8000, 18000, 20000, 20000], ratio: 2 }, // high dominates
     pitch: { value: "rpm", ramp: [0, 0, 0, 0], offset: 1, ratio: 1 } // no pitch change
